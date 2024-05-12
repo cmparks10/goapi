cd6367ff25587ab94d0d761fb35eed74852d871a
Corey Parks <cmparks10@gmail.com>
2024-05-11 20:22:55 -0400

    building the api

---
+++ cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"goapi/internal/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GO API Service")
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
+
+}
